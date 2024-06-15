85708a381b25f98522e93137e570a2931ba5a8db
theimdall <enes.gdeveloper@gmail.com>
2024-06-15 19:37:20 +0300

    Project Files Upload

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # findmydns
 The Software provides to Find & Set the Best Dns Server for you internet.
 
+[maiMenu]
+
 For business or questions => theimdall@outlook.com
--- findmydns.py
+++ findmydns.py
@@ -8,14 +8,14 @@ dns_server1 = []
 dns_server2 = []
 dns_serverProvider = []
 ms_values = []
-ms_vB_id = [0]
+ms_best_id = [0]
 dns_File="dns_servers.txt"
 
 
 def menu():
     is_Connected = is_connected()
     if is_Connected:
-        dns_serverCurrent,dns_serverCurrentIp = dns_checkerCurrent()
+        dns_serverCurrent,dns_serverCurrentIp = findDnsCurrent()
     else:
         dns_serverCurrent,dns_serverCurrentIp = "Not Connected",""
     
@@ -51,29 +51,29 @@ def menu():
     is_Connected = is_connected()
     if option_id == 0 and is_Connected:
         if os.path.exists(dns_File):
-            read_dnsFile()
-            dns_analysis()
+            readDnsFile()
+            analysisDns()
         else:
             dns_FileNew = open("dns_servers.txt", "w")
             dns_FileNew.write("8.8.8.8,8.8.4.4,Google\n1.1.1.1,1.0.0.1,Cloudflare\n9.9.9.9,149.112.112.112,Quad9\n208.67.222.222,208.67.220.220,OpenDNS Home\n94.140.14.14,94.140.15.15,AdGuard DNS\n185.228.168.9,185.228.169.9,Clean Browsing\n76.76.19.19,76.223.122.150,Alternate DNS\n77.88.8.8,77.88.8.1,Yandex\n8.26.56.26,8.20.247.20,Comodo Secure\n76.76.2.0,76.76.10.0,Control D")
             dns_FileNew.close()
             print("dns_servers.txt file is not exist. New One Created!")
-            read_dnsFile()
-            dns_analysis()
+            readDnsFile()
+            analysisDns()
     elif option_id == 1:
         dns_server1User = input("DNS1(ex. 8.8.8.8):")
         dns_server2User = input("DNS2(ex. 8.8.4.4):")
-        dns_setU(dns_server1User,dns_server2User)
+        setDnsUserInput(dns_server1User,dns_server2User)
     elif option_id == 2:
-        dns_reset()
+        resetDns()
     elif option_id == len(option_list)-1:
         exit()
         
-def dns_analysis():
+def analysisDns():
     for id,dns_s in zip(range(0,len(dns_server1)),dns_server1):
         is_Connected = is_connected()
         if is_Connected:
-            ms_values.append(dns_checker(dns_s))
+            ms_values.append(testDns(dns_s))
         else:
             exit()
         #print(dns_serverProvider[id],f":{ms_values[id]}ms")
@@ -84,56 +84,56 @@ def dns_analysis():
         print(f"a:{a}")
         if ms_vB [a] < ms_vB[a+1] and a < len(ms_values)-1:
             if ms_vB[a] != ms_vB[a-1]:
-                ms_vB_id[0] = a
-                print("best_id:",ms_vB_id)
+                ms_best_id[0] = a
+                print("best_id:",ms_best_id)
                 ms_vB[a+1] = ms_vB[a]
-            elif ms_vB[ms_vB_id[0]] == ms_vB[a]:
-                print("best_id:",ms_vB_id)
+            elif ms_vB[ms_best_id[0]] == ms_vB[a]:
+                print("best_id:",ms_best_id)
                 ms_vB[a+1] = ms_vB[a]
             print(f"a={a},{ms_vB[a]} < {ms_vB[a+1]}")   
-            print("best_idSame:",ms_vB_id)
+            print("best_idSame:",ms_best_id)
         elif ms_vB[a] > ms_vB[a+1] and a < len(ms_values)-2:
-            ms_vB_id[0] = a+1
+            ms_best_id[0] = a+1
         
         else:
-            ms_vB_id[0] = a+1
-            print(f"elseWorked, a:{a},best_id:{ms_vB_id}")
+            ms_best_id[0] = a+1
+            print(f"elseWorked, a:{a},best_id:{ms_best_id}")
         print(f"a={a}",ms_vB)
             
             
-    print("best_id:",ms_vB_id)
+    print("best_id:",ms_best_id)
     print("ms_values:",ms_values)
     for a in range(0,len(ms_values)):
-        if ms_values[ms_vB_id[0]] == ms_values[a]:
+        if ms_values[ms_best_id[0]] == ms_values[a]:
             print("a")
-            ms_vB_id.append(a)
-        print("ms_vB_id",ms_vB_id)
-    if len(ms_vB_id) > 2:
+            ms_best_id.append(a)
+        print("ms_best_id",ms_best_id)
+    if len(ms_best_id) > 2:
         print("Best Dns Servers:", end="")
-        for a,b in zip(ms_vB_id[1:],range(1,len(ms_vB_id))):
+        for a,b in zip(ms_best_id[1:],range(1,len(ms_best_id))):
                 print(f"{b}){dns_serverProvider[a]}:{ms_values[a]}ms", end=" ")
         #print(f"{a}){dns_serverProvider[a]}:{ms_values[a]}ms", end=" ")
     else:
-        print("BestDnsServer:",dns_serverProvider[ms_vB_id[0]],":",ms_values[ms_vB_id[0]])
+        print("BestDnsServer:",dns_serverProvider[ms_best_id[0]],":",ms_values[ms_best_id[0]])
     print("")
     isUserChangeDns = None
     while isUserChangeDns != "yes" and isUserChangeDns != "no":
         isUserChangeDns = str(input("Do you want to change your DNS settings(yes/no) :"))
     if isUserChangeDns == "yes":
-        if len(ms_vB_id) > 2:
+        if len(ms_best_id) > 2:
             dns_change_id = int(input("Which?:"))
             print(f"dns_change_id:{dns_change_id}")
-            for a in range(1,len(ms_vB_id)):
+            for a in range(1,len(ms_best_id)):
                 print(f"loopWorked:{a} times")
                 print(f"dns_change_id:{dns_change_id}")
                 if dns_change_id == a:
-                    dns_change_id = ms_vB_id[a]
+                    dns_change_id = ms_best_id[a]
                     print("choosed:",dns_change_id)
                     print(f"DnsChoosed:{dns_serverProvider[dns_change_id]},{ms_values[dns_change_id]}, Informations:{dns_server1[dns_change_id]},{dns_server2[dns_change_id]}")
-                    dns_set(dns_change_id)
+                    setDns(dns_change_id)
         else:
-            print(f"DnsChoosed:{dns_serverProvider[ms_vB_id[0]]},{ms_values[ms_vB_id[0]]}, Informations:{dns_server1[ms_vB_id[0]]},{dns_server2[ms_vB_id[0]]}")
-            dns_set(ms_vB_id[0])
+            print(f"DnsChoosed:{dns_serverProvider[ms_best_id[0]]},{ms_values[ms_best_id[0]]}, Informations:{dns_server1[ms_best_id[0]]},{dns_server2[ms_best_id[0]]}")
+            setDns(ms_best_id[0])
     elif isUserChangeDns == "no":
         print("Okey.")
                 
@@ -141,7 +141,7 @@ def dns_analysis():
         print("Incorrect choose") 
         menu()
         
-def read_dnsFile():
+def readDnsFile():
     with open(dns_File,"r") as dns_FileSystem:
         for line in dns_FileSystem:
             parts = line.strip().split(',')
@@ -152,7 +152,7 @@ def read_dnsFile():
             
     return dns_server1,dns_server2,dns_serverProvider
 
-def dns_checker(dns_serv):
+def testDns(dns_serv):
     command = ["ping", dns_serv]
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
     outputCommand, _ = process.communicate()
@@ -166,7 +166,7 @@ def dns_checker(dns_serv):
     return ms_vC
 
 
-def dns_checkerCurrent():
+def findDnsCurrent():
     command = "nslookup google.com"
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
     outputCommand, _ = process.communicate()
@@ -181,9 +181,8 @@ def dns_checkerCurrent():
     serverIpInfo = outputCommandDecoded[output_serverIpInfoLocation:output_midText.start()].replace("\n","").replace("\r","").strip()
     return serverNameInfo,serverIpInfo
 
-def dns_set(dns_id):
+def setDns(dns_id):
     commands = f'/k netsh interface ipv4 delete dnsservers "Wi-Fi" all & netsh interface ipv4 delete dnsservers "Ethernet" all & netsh interface ipv4 add dnsserver "Wi-Fi" address={dns_server1[dns_id]} index=1 & netsh interface ipv4 add dnsserver "Wi-Fi" address={dns_server2[dns_id]} index=2 & netsh interface ipv4 add dnsserver "Ethernet" address={dns_server1[dns_id]} index=1 & netsh interface ipv4 add dnsserver "Ethernet" address={dns_server2[dns_id]} index=2 & exit'
-    #commandsT = f'/k netsh interface ipv4 delete dnsservers "Wi-Fi" all & netsh interface ipv4 delete dnsservers "Ethernet" all & netsh interface ipv4 add dnsserver "Wi-Fi" address={dns_server1[dns_id]} index=1 & netsh interface ipv4 add dnsserver "Wi-Fi" address={dns_server2[dns_id]} index=2 & netsh interface ipv4 add dnsserver "Ethernet" address={dns_server1[dns_id]} index=1 & netsh interface ipv4 add dnsserver "Ethernet" address={dns_server2[dns_id]} index=2 & netsh interface ip show config'
     ctypes.windll.shell32.ShellExecuteW(
             None,
             u"runas",
@@ -195,9 +194,8 @@ def dns_set(dns_id):
     input("Process Completed.Press any button for Menu...")
     menu()
 
-def dns_setU(dns_s1,dns_s2):
+def setDnsUserInput(dns_s1,dns_s2):
     commands = f'/k netsh interface ipv4 delete dnsservers "Wi-Fi" all & netsh interface ipv4 delete dnsservers "Ethernet" all & netsh interface ipv4 add dnsserver "Wi-Fi" address={dns_s1} index=1 & netsh interface ipv4 add dnsserver "Wi-Fi" address={dns_s2} index=2 & netsh interface ipv4 add dnsserver "Ethernet" address={dns_s1} index=1 & netsh interface ipv4 add dnsserver "Ethernet" address={dns_s2} index=2 & exit'
-    #commandsT = f'/k netsh interface ipv4 delete dnsservers "Wi-Fi" all & netsh interface ipv4 delete dnsservers "Ethernet" all & netsh interface ipv4 add dnsserver "Wi-Fi" address={dns_server1[dns_id]} index=1 & netsh interface ipv4 add dnsserver "Wi-Fi" address={dns_server2[dns_id]} index=2 & netsh interface ipv4 add dnsserver "Ethernet" address={dns_server1[dns_id]} index=1 & netsh interface ipv4 add dnsserver "Ethernet" address={dns_server2[dns_id]} index=2 & netsh interface ip show config'
     ctypes.windll.shell32.ShellExecuteW(
             None,
             u"runas",
@@ -209,7 +207,7 @@ def dns_setU(dns_s1,dns_s2):
     input("Process Completed. Press any button for Menu...")
     menu()
     
-def dns_reset():
+def resetDns():
     commands = f'/k netsh interface ipv4 delete dnsservers "Wi-Fi" all & netsh interface ipv4 delete dnsservers "Ethernet" all  & exit'
     #commandsT = f'/k netsh interface ipv4 delete dnsservers "Wi-Fi" all & netsh interface ipv4 delete dnsservers "Ethernet" all & netsh interface ip show config'
     ctypes.windll.shell32.ShellExecuteW(
@@ -232,6 +230,5 @@ def is_connected():
         return False
 
 
-    
 
 menu()
